7466d185a9fc0e23c897bc2cd37ba7944584535f
john Caropino <jccaropino7786@live.com>
2022-11-20 15:57:22 -0800

    commit tree c8fcf333657d884a6dd5ae76a27c791d451b31f5

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  
+  if (distance > 0 && distance <= 400){
+    //gives passenger free ride if less than or equal to 400 feet
+    return 'This one is on me!';
+  }
+  else if (distance > 400 && distance <= 2000){
+    //charges 20 dollars for a distance between 400 and 2000 feet
+    return'That will be twenty bucks.';
+  } 
+  else if (distance > 2000 && distance <= 2500){
+  //charges 30 dollars for a distance between 2000 & 2500
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if(distance > 2500){
+  //no rides over 2500
+    return 'No can do.'
+  }
+}
+  
+function ternaryCheckCity(city){
+ return (city === "NYC") ? "Ok, sounds good." : "No go.";  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(charm){
+  switch(charm){
+    case 'generous':
+     return charm = "Thank you so much.";
+      
+    case 'not as generous':
+      return charm = "Thank you.";
+      
+    default:
+      return charm = "Bye.";
+      
+  }
+  
 }
